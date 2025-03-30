8519046346bb60f5804bc40a6a3d30662008220e
kinocoffeeblack <kinocoffeeblack@gmail.com>
2025-03-30 18:38:31 +0900

    mycustomApiの追加

--- packages/cdk/lambda/utils/api.ts
+++ packages/cdk/lambda/utils/api.ts
@@ -2,12 +2,14 @@ import bedrockApi from './bedrockApi';
 import bedrockAgentApi from './bedrockAgentApi';
 import bedrockKbApi from './bedrockKbApi';
 import sagemakerApi from './sagemakerApi';
+import mycustomApi from './mycustomApi';
 
 const api = {
   bedrock: bedrockApi,
   bedrockAgent: bedrockAgentApi,
   bedrockKb: bedrockKbApi,
   sagemaker: sagemakerApi,
+  mycustom: mycustomApi,
 };
 
 export default api;
